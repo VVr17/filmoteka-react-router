90f50583adc0b6b912c566b043199d32209d5555
Vira Voronova <vera.voronova@hotmail.com>
2022-10-31 10:48:34 +0100

    Api requests done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { Api } from './services/Api';
+const apiService = new Api();
+apiService.getGenres();
+apiService.getTrendingMovies();
+apiService.searchByKeyWord('matrix');
+apiService.getMovieDetailsById(603);
+apiService.getMovieCreditsById(603);
+apiService.getMovieReviewsById(603);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
